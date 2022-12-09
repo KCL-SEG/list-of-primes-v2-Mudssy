0a052f1dae1ba30d876d626c784937f3510ac2ed
Mudassir Ahmad <smahmad2002@gmail.com>
2022-12-09 10:42:33 +0000

    complete

--- primes.py
+++ primes.py
@@ -3,4 +3,27 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <=0:
+        raise ValueError
+    
+    counter = 0
+    num = 2
+
+    while counter != number_of_primes:
+        is_prime = True
+        i = 0
+        length = len(list)
+        while i < length and is_prime:
+            if num % list[i] == 0:
+                is_prime =  False
+            i += 1
+        
+        if is_prime:
+            list.append(num)
+            counter += 1
+
+        num += 1
+
+
     return list
+
